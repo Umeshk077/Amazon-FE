9682eec8bd1db9e96a78a913e0b66d03eb8bb52c
Umeshk077 <umeshkaveri92260@gmail.com>
2024-10-22 21:13:52 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "amazon-cluster-s3-bucket"  # Replace with your actual S3 bucket name
+    bucket = "amazon-project-s3-bucket"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
     region = "us-east-1" 
   }
